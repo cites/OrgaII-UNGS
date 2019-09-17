2acd183a2901e1addfb21529e3b4aa6d16bd13a3
lapassesungs <pintososcar@gmail.com>
2019-09-17 16:47:38 -0300

    ejercicio 4 Rocio

---
+++ Introducción a C/Ejercicio4R.c
@@ -0,0 +1,26 @@
+//Calcular factorial de un número
+#include <stdio.h>
+
+int main ()
+{
+    long int n;
+    long int resultado;
+
+    printf ("Ingrese un número entero: \n");
+    scanf ("%ld", &n);
+
+    // Calcular el factorial del numero solicitado
+    if (n == 0)
+        resultado = 0;
+    else
+        resultado = 1;
+    
+    while(n > 1) {
+       resultado *= n;
+       //printf(" %ld x",n);
+       n--;
+    }
+    printf("Su factorial es: %ld\n\n",resultado);
+
+    return 0;
+}
