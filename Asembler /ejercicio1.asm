21f03f0c43fb35ac2f6448d1eec95ea658569263
Oscar <pintososcar@gmail.com>
2019-09-24 21:28:10 -0300

    ejercicio 1 y 2

--- Asembler /ejercicio1.asm
+++ Asembler /ejercicio1.asm
@@ -1,22 +1,71 @@
-section data;
+%include "io.inc"
 
-    N1 db 10 ;entero sin signo de 8 bits
-    N2 db 234 ;entero sin signo de 8bits
+section .text
+
+    T dw "Hola loco",10,13,'$'
     
-    N3 dw 255 ;entero de 16 bits
-    N4 dw 1348 ;entero de 16bits
+section .data
+
+    N1 db 254d  ;8 bits
+    N2 db 1d
     
-    N5 dd 34099 ; entero de 32 bits
-    N6 dd 678367 ; entero de 32 bits
+    N3 dw 354d  ;16 bits
+    N4 dw 124d
     
-    N7 dq 33453456 ;entero de 64 bits
-    N8 dq 560952 ; entero de 64 bits
+    N5 dd 12345d  ;32 bits
+    N6 dd 456932d
     
-    F1 dd 457367,094 ; punto flotante de presicion  simple
+    N7 dq 56789332533d ;64 bits
+    N8 dq 54398044568d   
     
-    T dd 'Hola Mundo'; cadena de carateres
+    F1 dd 123.567 ; Punto flotante de precisión simple
+    
     
 
 
+
+global CMAIN
+CMAIN:
+    mov ebp, esp; for correct debugging
+    ;write your code here
+    
+    PRINT_STRING "Variable T  : " 
+    PRINT_STRING T
+    NEWLINE
+    
+    PRINT_STRING "Variable N1  : "
+    PRINT_UDEC 1,N1
+    NEWLINE
+    
+    PRINT_STRING "Variable N2  : "
+    PRINT_UDEC 1,N2
+    NEWLINE
+    
+    PRINT_STRING "Variable N3  : "
+    PRINT_UDEC 2,N3
+    NEWLINE
+    
+    PRINT_STRING "Variable N4  : "
+    PRINT_UDEC 2,N4
+    NEWLINE
+    
+    PRINT_STRING "Variable N5  : "
+    PRINT_UDEC 4,N5
+    NEWLINE
+    
+    PRINT_STRING "Variable N6  : "
+    PRINT_UDEC 4,N6
+    NEWLINE
+    
+    PRINT_STRING "Variable N7  : "
+    PRINT_UDEC 4,N7
+    NEWLINE
+    
+    PRINT_STRING "Variable N8  : "
+    PRINT_DEC 4,N8
+    NEWLINE
+    
+    xor eax, eax
+    ret
 
     
