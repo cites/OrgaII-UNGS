ab6dba2292ecc643862f3b0577ae21fd8cc19f1a
Oscar <pintososcar@gmail.com>
2019-10-02 15:33:14 -0300

    listo a

--- fpu/practicafpu.asm
+++ fpu/practicafpu.asm
@@ -7,7 +7,9 @@ extern printf
 section .data
 
     fmt db "%f", 10, 0
-    
+
+    c dq -1.0E10
+    d dq 6.02E23
 
 section .text
 
@@ -18,6 +20,9 @@ CMAIN:
     fldz ; carga 0.0
     
     fld1 ; carga 1.0
+    fld qword [c]; carga -1.0E10
+    fld qword [d]
+
     fldpi ; carga pi
     
     
