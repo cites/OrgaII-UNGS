bb943729f9443032386bfc2cc7e489e98cfc8ab1
Oscar <pintososcar@gmail.com>
2019-10-05 17:20:14 -0300

    Auto stash before merge of "master" and "origin/master"

--- fpu/practicafpu.asm
+++ fpu/practicafpu.asm
@@ -5,7 +5,7 @@ extern printf
 
 section .data
 
-    fmt db "%f", 10, 0
+    fm db "%f", 10, 0
 
     c dq -1.0E10
     d dq 6.02E23
@@ -16,18 +16,21 @@ main:
     mov ebp, esp; for correct debugging
         ;write your code here
     
-    fldz ; carga 0.0
+        fldz ; carga 0.0
     
-    fld1 ; carga 1.0
-    fld qword [c]; carga -1.0E10
-    fld qword [d]
+        fld1 ; carga 1.0
+        fld qword [c]; carga -1.0E10
+        fld qword [d]
 
-    fldpi ; carga pi
+        fldpi ; carga pi
+        fst qword [c]
+
     
     
-    ;push fmt
-    ;call printf
-    ;add ESP, 12
+    push fm
+    call printf
+    add ESP, 12
+    
     
     xor eax, eax
     ret
